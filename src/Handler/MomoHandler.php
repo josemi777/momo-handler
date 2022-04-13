92093ad8770ebb263a7641dcabe50141cb86ddbe
Jose Miguel Sanchez Ruiz <joseybe@hotmail.com>
2022-04-13 16:46:15 +0200

    v1.0.0 Beta

--- src/Handler/MomoHandler.php
+++ src/Handler/MomoHandler.php
@@ -1,11 +1,39 @@
 <?php
 
-namespace Handler;
+namespace MomoHandler\Handler;
+
+use Illuminate\Support\Facades\Http;
+
+define('MOMO_BRAIN_URL', env('MOMO_BRAIN_URL'));
 
 class MomoHandler
 {
-    public static function world()
+    public static function CallMicro($microservice, $action, $method, $params)
     {
-        return 'Hello World, Composer!';
+        try {
+            $url = self::get_microservice_url($microservice, $action);
+            $response = Http::post($url, $params);
+            return response($response->body());
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    private static function get_microservice_url($microservice, $action)
+    {
+        try {
+            if (!constant('MOMO_BRAIN_URL')) {
+                throw new \Exception("MOMO_BRAIN_URL not found. Please configure it on your environment file.");
+            }
+            $response = Http::post(constant('MOMO_BRAIN_URL'), [
+                'microservice' => $microservice,
+                'action' => $action
+            ]);
+            return $response->body();
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
     }
 }
